04f09097595be599fbf22d679504858f62e95425
adithyareddy369-Byte <adithyapreddy768@gmail.com>
2025-10-28 18:03:35 +0530

    Updated Signup.tsx

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import SignupForm from '@components/auth/SignupForm';
+
+const Signup = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+      <SignupForm />
+    </div>
+  );
+};
+
+export default Signup;
