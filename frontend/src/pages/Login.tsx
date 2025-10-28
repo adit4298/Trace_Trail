32b23984d0a93557382ef11163218f3d55a37057
adithyareddy369-Byte <adithyapreddy768@gmail.com>
2025-10-28 18:02:58 +0530

    Updated Login.tsx

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '@components/auth/LoginForm';
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
