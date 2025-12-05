01a8e99706ca2e02b2279bea27329a1c81d0a694
adit4298 <adit.padman17@gmail.com>
2025-12-05 09:50:19 +0530

    frontend

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wbXzEUglda6CTqAKbg/mFyEDysu8CjWNFC0yYJLZK7k=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wt/tVyS5S8ZreX8o4Dts+6McgSW1eNA7/2osc0Da6q8=\"\n}"
